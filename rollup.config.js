43a319f187069861d4b24d0890b845bb729fab7b
Bright Philip <chibuzorphilip2001@gmail.com>
2024-12-27 17:48:52 +0100

    added missing fields from package json

--- rollup.config.js
+++ rollup.config.js
@@ -5,20 +5,33 @@ import dts from 'rollup-plugin-dts'
 
 const packagejson = require('./package.json')
 
-export default {
-    input: 'src/index.ts',
-    output: [
-        {
-            file: packagejson.main,
-            format: 'cjs',
-            sourcemap: true,
-        },
+export default [
+    {
+        input: 'src/index.ts',
+        output: [
+            {
+                file: packagejson.main,
+                format: 'cjs',
+                sourcemap: true,
+            },
 
-        {
-            file: packagejson.module,
-            format: 'esm',
-            sourcemap: true,
-        },
-    ],
-    plugins: [typescript(), resolve(), commonjs()]
-  };
+            {
+                file: packagejson.module,
+                format: 'esm',
+                sourcemap: true,
+            },
+        ],
+        plugins: [typescript({tsconfig: './tsconfig.json'}), resolve(), commonjs()]
+    },
+    {
+        input: 'dist/esm/types/index.d.ts',
+        output: [
+            {
+                file: 'dist/index.d.ts',
+                format: 'esm',
+            },
+        ],
+        plugins: [dts()],
+    }
+
+];
